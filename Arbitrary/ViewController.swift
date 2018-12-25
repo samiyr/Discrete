edd7ca2d158d1313f311e2c137771ee26a8b32b6
Sami <imas145online1@icloud.com>
2018-12-25 12:11:07 +0200

    Initial Commit

---
+++ Arbitrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arbitrary
+//
+//  Created by Sami Yrjänheikki on 25/12/2018.
+//  Copyright © 2018 Sami Yrjänheikki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
